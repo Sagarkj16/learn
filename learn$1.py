3bcce6c9a93490a2ba0075a533bc01daa0b5e67a
Sagarkj16 <sagarkj16@gmail.com>
2023-05-06 10:12:37 +0530

    added test.html file

--- learn$1.py
+++ learn$1.py
@@ -1,1 +1,19 @@
+#binary sorting
 print("hello")
+nums=[1,2,7,6,5,4,33,3,5,6,7,77]
+def sort(nums):
+    for i in range(len(nums)-1):
+        minpos=i
+        for j in range(i,len(nums)):
+            if nums[j]<nums[minpos]:
+                minpos=j
+
+
+
+        else:
+            temp=nums[i]
+            nums[i]=nums[minpos]
+            nums[minpos]=temp
+
+sort(nums)
+print(nums)
